e2e178f411eb14e5622065936ca3389d65618a7e
Chad Fairlie <chad.fairlie2004@gmail.com>
2024-03-21 23:00:16 +0200

    Changed some stuff - Roll back here if white box fails

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace CloudDevPOE.Controllers
             return View();
         }
 
+        public IActionResult Main()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,9 +1,9 @@
 namespace CloudDevPOE.Models
 {
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
 
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
 }
