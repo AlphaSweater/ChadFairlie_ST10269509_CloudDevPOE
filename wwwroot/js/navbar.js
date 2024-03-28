0ca3431fa4627baab390fef62bafa8272b28b185
Chad Fairlie <chad.fairlie2004@gmail.com>
2024-03-28 17:57:17 +0200

    `Redesigned website layout and updated animations`
     Removed sidebar and dropdown menu functionalities, and updated bubble animations with a new color scheme and animation style. Replaced `animated.css` with `animated-background.css` and renamed `.container` class to `#bg-container`. Also, removed `sidebar.css`, `navbar.js`, and `sidebar.js` files. Made corresponding changes in `_Layout.cshtml`.

--- wwwroot/js/navbar.js
+++ wwwroot/js/navbar.js
@@ -1,20 +1,3 @@
 ﻿/* ==================== NAVIGATION BAR CSS ==================== */
 
-/* ===== Drop Down Menu ===== */
-document.addEventListener("click", e => {
-    const isDropdownButton = e.target.matches("[data-dropdown-button]")
-    if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
-
-    let currentDropdown
-    if (isDropdownButton) {
-        currentDropdown = e.target.closest("[data-dropdown]")
-        currentDropdown.classList.toggle("active")
-    }
-
-    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
-        if (dropdown === currentDropdown) return
-        dropdown.classList.remove("active")
-    })
-})
-
 /* ===== TITLE ===== */
