3d606a8f65f7cff5694155b7f62750b24d25cab1
Chad Fairlie <chad.fairlie2004@gmail.com>
2024-04-04 00:06:47 +0200

    Added `tblUsers` class and SQL Server connection
    Added a new `tblUsers` class in the `CloudDevPOE.Models` namespace with properties for `Name`, `Surname`, and `PhoneNo`. Also, updated the `CloudDevPOE.csproj` file to include a new package reference to `System.Data.SqlClient` version `4.8.6` for SQL Server database connections. A static connection string and `SqlConnection` object have been defined for connecting to an Azure-hosted SQL Server database. The `tblUsers` class includes a `insert_User(tblUsers m)` method for inserting new users into the `UserTBL` table in the database.

---
+++ Models/tblUsers.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.SqlClient;
+
+namespace CloudDevPOE.Models
+{
+    public class tblUsers
+    {
+        public static string conString = "Server=tcp:st10269509-server.database.windows.net,1433;Initial Catalog=ST10269509-DB;Persist Security Info=False;User ID=AlphaSweater;Password=N@l@2004;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+        public static SqlConnection con = new SqlConnection(conString);
+
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? PhoneNo { get; set; }
+
+        public int insert_User(tblUsers m)
+        {
+            try
+            {
+                string sql = "INSERT INTO UserTBL (UserName, UserSurname, UserPhoneNo) VALUES (@UserName, @UserSurname, @UserPhoneNo)";
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("@UserName", m.Name);
+                cmd.Parameters.AddWithValue("@UserSurname", m.Surname);
+                cmd.Parameters.AddWithValue("@UserPhoneNo", m.PhoneNo);
+
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                con.Close();
+                return rowsAffected;
+            } catch (System.Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
