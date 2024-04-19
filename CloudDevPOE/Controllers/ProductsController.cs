d47b138cff86cf52884999db1d126e9f9167b86d
Chad Fairlie <chad.fairlie2004@gmail.com>
2024-04-19 16:12:57 +0200

    Moved MyWork to the Products View folder

--- CloudDevPOE/Controllers/ProductsController.cs
+++ CloudDevPOE/Controllers/ProductsController.cs
@@ -4,32 +4,7 @@ namespace CloudDevPOE.Controllers
 {
     public class ProductsController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult HomeDecorPage()
-        {
-            return View();
-        }
-        public IActionResult JewelryPage()
-        {
-            return View();
-        }
-        public IActionResult MetalArtPage()
-        {
-            return View();
-        }
-        public IActionResult PotteryPage()
-        {
-            return View();
-        }
-        public IActionResult TextilesPage()
-        {
-            return View();
-        }
-        public IActionResult Woodwork()
+        public IActionResult MyWork()
         {
             return View();
         }
